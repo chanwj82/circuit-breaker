97e45e6129168ab2fa2d18cba2af6ae18f5bd1ed
jc.lee <jc.lee@navercorp.com>
2021-02-01 15:00:09 +0900

    Remove unused directory

--- Resilience4j/src/main/java/me/jongchan/resilience4j/CustomException.java
+++
@@ -1,8 +0,0 @@
-package me.jongchan.resilience4j;
-
-public class CustomException extends Exception {
-
-  public CustomException(String message) {
-    super(message);
-  }
-}
--- Resilience4j/src/main/java/me/jongchan/resilience4j/RetryException.java
+++
@@ -1,8 +0,0 @@
-package me.jongchan.resilience4j;
-
-public class RetryException extends Exception {
-
-  public RetryException(String message) {
-    super(message);
-  }
-}
